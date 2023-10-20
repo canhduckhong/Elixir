a8a54da78f9a8155c5b992e189b4296ee8d795e6
Chris Khong <khongduccanh.315@gmail.com>
2023-10-20 11:49:22 +0200

    add test

--- algorithm/test/selection_sort_test.exs
+++ algorithm/test/selection_sort_test.exs
@@ -13,4 +13,8 @@ defmodule SelectionSortTest do
   test "sorts a list of integers" do
     assert SelectionSort.sort([1, 5, 4, 3, 10, 12]) == [1, 3, 4, 5, 10, 12]
   end
+
+  test "sorts a list of float numbers" do
+    assert SelectionSort.sort([-1, 5.1, 4.2, 4.1, 10.0, 12]) == [-1, 4.1, 4.2, 5.1, 10.0, 12]
+  end
 end
