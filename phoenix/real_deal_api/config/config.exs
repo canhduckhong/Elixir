0059c9d73eae290efe9e085a8ced7c373e09655c
Chris Khong <khongduccanh.315@gmail.com>
2023-10-26 19:31:47 +0200

    Authenticate using Guardian and Bcrypt

--- phoenix/real_deal_api/config/config.exs
+++ phoenix/real_deal_api/config/config.exs
@@ -27,6 +27,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :real_deal_api, RealDealApiWeb.Auth.Guardian,
+  issuer: "real_deal_api",
+  secret_key: "z9Sc27h3dJbLW2lQHupRQXBzSU0Sh8ro45GM8EB/2pZOpipPzn1ZHUgYp5wLW1wv"
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
