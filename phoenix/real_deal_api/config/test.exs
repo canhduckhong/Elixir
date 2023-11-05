b64d41eefa25f6c549b8a1a0839cc7e3196dad1c
Chris Khong <khongduccanh.315@gmail.com>
2023-11-05 21:31:46 +0100

    Tests for Account and User fields' types

--- phoenix/real_deal_api/config/test.exs
+++ phoenix/real_deal_api/config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :real_deal_api, RealDealApi.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "chriskhong",
+  password: "12345678",
   hostname: "localhost",
   database: "real_deal_api_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
